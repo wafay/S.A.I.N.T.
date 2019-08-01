38783eeee94aaaac2c59428cdfe01f14ce85ba0f
Francisco <BlackFrank@users.noreply.github.com>
2019-08-01 08:31:20 -0400

    Update saint.js

--- saint.js
+++ saint.js
@@ -33,8 +33,6 @@ if(command === "ping") {
 }
 
 if(command === "say") {
-    // Makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
